13ddd36bc9c2765cfa4324ac0ccb08ed074858a4
Rebecca <r.stelter@gmx.com>
2022-11-25 15:08:51 +0100

    started members page

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect
-from flaskblog import app
+from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
 
@@ -28,9 +28,15 @@ def home():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        # hashing turns password (or any other plaintext) into
+        # a short, random looking string using encryption algorithm
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password= hashed_password)
+        db.session.add(user)
+        db.session.commit()
         # Redirects to home.html and creates success message
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -39,7 +45,11 @@ def login():
     if form.validate_on_submit():
         if form.email.data == 'admin@super.com' and form.password.data == 'password':
             flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+            return redirect(url_for('members'))
         else:
             flash('Login unseccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+@app.route('/members')
+def members():
+    return render_template('members.html')
